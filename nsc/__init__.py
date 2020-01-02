6370a739157baaa7dbdd7b0f093bf4592c3c48b6
Peter P. Marks <peter.marks@onepeloton.com>
2020-01-02 14:59:42 -0500

    Enable Poetry

--- nsc/__init__.py
+++ nsc/__init__.py
@@ -2,3 +2,5 @@ from .core import Core
 from .deck import Deck
 from .hand import Hand
 from .handler import Handler
+
+__version__ = '0.1.0'
---
+++ tests/test_nsc_game.py
@@ -0,0 +1,5 @@
+from nsc import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
